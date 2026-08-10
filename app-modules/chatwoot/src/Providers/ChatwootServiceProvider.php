0f9fc0028957f1daf091cae929fe800c8df8be06
Sumon Ahmed <bdsumon4u@gmail.com>
2026-08-11 03:09:52 +0600

    feat: implement modular architecture with new chatwoot and n8n app modules

---
+++ app-modules/chatwoot/src/Providers/ChatwootServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hotash\Chatwoot\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ChatwootServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
---
+++ app-modules/n8n/src/Providers/N8nServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hotash\N8n\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class N8nServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
